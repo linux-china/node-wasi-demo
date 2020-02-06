e8f8cdad3723e31c0b6c7d7e7474f3b400a8349d
linux_china <libing.chen@gmail.com>
2020-02-05 20:06:31 -0800

    node wasi launch

---
+++ wasi-launch.js
@@ -0,0 +1,18 @@
+'use strict';
+const fs = require('fs');
+const { WASI } = require('wasi');
+const wasi = new WASI({
+    args: process.argv,
+    env: process.env,
+    preopens: {
+        '/sandbox': '/Users/linux_china/data/sandbox'
+    }
+});
+const importObject = {wasi_unstable: wasi.wasiImport};
+
+(async () => {
+    const wasm = await WebAssembly.compile(fs.readFileSync('./example.wasm'));
+    const instance = await WebAssembly.instantiate(wasm, importObject);
+
+    wasi.start(instance);
+})();
